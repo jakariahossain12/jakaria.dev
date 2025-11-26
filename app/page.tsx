3d47a0de2a8db2495c19729cd18cddedd7e8916c
Mdjakaria53 <kjakaria53@gmail.com>
2025-11-26 22:07:18 +0300

    crate about component and modify gooeynev page and css modify layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/components/Hero"
+import About from "@/components/About"
 const page = () => {
   return (
     <section>
       
       <Hero/>
+      <About/>
+
       
     </section>
 
