4a6ac6feea044b5b16e2bf29e6a0ce632e79bb00
Mdjakaria53 <kjakaria53@gmail.com>
2025-11-26 23:09:00 +0300

    create skills component and add clarehover from react bits

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero"
 import About from "@/components/About"
+import Skills from "@/components/Skills"
 const page = () => {
   return (
     <section>
       
       <Hero/>
       <About/>
+      <Skills/>
 
       
     </section>
